28b84f3660cb4368f2bede05e8333c8bac49dc83
Candice Tate <67024645+candicetate@users.noreply.github.com>
2020-07-28 20:12:30 -0600

    Added files & scripts

---
+++ src/script.js
@@ -0,0 +1,6 @@
+var user = prompt("What is your first name?");
+var last = prompt("What is your last name?");
+var age = prompt("How old are you, " + user + "?");
+alert(
+  "Your name is " + user + " " + last + ", and you are " + age + " years old."
+);
